83b8a63ab2b4eea80af6652288fd12524828789c
Enrico-N <ngenricok@gmail.com>
2022-05-12 18:06:48 -0700

    Update Website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>Hello, I'm Enrico</h1>
+        <h2>Software Developer</h2>
+
+        <div id='Social-icons'>
+        <a href="https://www.linkedin.com/in/enrico-ng/" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} size="2x"/>
         </a>
+        <a href="https://github.com/Enrico-N" target="_blank">
+        <FontAwesomeIcon icon={faGithub} size= "2x"/>
+        </a>
+        </div>
+        
       </header>
     </div>
   );
